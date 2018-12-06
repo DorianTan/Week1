7bb1c689643cb9ce54c78ad2e5ad2a7c232462e3
Tan <doriantan7@gmail.com>
2018-12-06 13:07:59 +0100

    MoveTopDown/SpritesRygar

--- Assets/Scripts/PlayerMoveUpDown.cs
+++ Assets/Scripts/PlayerMoveUpDown.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveUpDown : MonoBehaviour {
+public class PlayerMoveUpDown : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private float speed;
+    private Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();  
+    }
+ 
+
+    void Move()
+    {
+        Vector2 velocity = new Vector2(Input.GetAxis("Horizontal") * speed*Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        rigid.velocity = velocity;
+    }
+
+    public void jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+    }
 }
