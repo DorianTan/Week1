f0d9f8bc70537883e242ccbfafb037749b76ee93
Ethan Cavadia <cavadiaethan@gmail.com>
2018-12-06 14:17:40 +0100

    Tesst

---
+++ Assets/Scripts/PlayerScripts/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private GameObject weapon;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+
+        }
+    }
+}
