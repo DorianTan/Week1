ebdd21639151fe433a83d4009194db4bec43f83f
Tan <doriantan7@gmail.com>
2018-12-08 12:58:53 +0100

    movement animation

--- Assets/Scripts/PlayerMoveUpDown.cs
+++ Assets/Scripts/PlayerMoveUpDown.cs
@@ -9,6 +9,8 @@ public class PlayerMoveUpDown : MonoBehaviour
     private float speed;
     private Rigidbody2D rigid;
 
+    public Animator animator;
+
 
     void Start()
     {
@@ -26,6 +28,10 @@ public class PlayerMoveUpDown : MonoBehaviour
     {
         Vector2 velocity = new Vector2(Input.GetAxis("Horizontal") * speed*Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
         rigid.velocity = velocity;
+        animator.SetFloat("Horizontal",velocity.x);
+        animator.SetFloat("Vertical", velocity.y);
+        animator.SetFloat("Magnitude",velocity.magnitude);
+
     }
 
     public void Jump()
